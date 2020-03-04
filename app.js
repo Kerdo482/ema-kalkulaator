a804301b91d2fde2db3b46d394b9a0d6538f6136
Robin Kukk <robin.kukk@ametikool.ee>
2020-03-04 15:16:34 +0200

    Basic functionality working

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-fetch('http://andmebaas.stat.ee/sdmx-json/data/PA634')
+
+function getinfo(){
+  // vars
+  var e = document.getElementById("jobSelect");
+  var selectnr = e.options[e.selectedIndex].value;
+
+  //var text = e.options[e.selectedIndex].text;
+  var jsinput = document.getElementById('testinput')
+  currenthrs = jsinput.value
+  //fetch start
+fetch('http://andmebaas.stat.ee/sdmx-json/data/PA627/' + selectnr + '.1.1+2/all?startTime=2010&endTime=2014&dimensionAtObservation=allDimensions')
     .then((response) => {
     return response.json()
   })
   .then((data) => {
     const statDiv = document.getElementById('root')
-    const testDiv = document.getElementById('test')
+    const totalDiv = document.getElementById('total')
+    const palkNr = data.dataSets[0].observations['0:0:0:1'][0]
     console.log(data)
+    statDiv.innerHTML = palkNr
+    totalDiv.innerHTML = palkNr * currenthrs
 
 
-    data.structure.dimensions.series[2].values.forEach(stats => {
-        let test2Div = document.createElement('option')
-        test2Div.innerHTML = stats.name
-        testDiv.append(test2Div)
-
-
+      
 
     });
-});
-  // ============================
+  }
+  //button
+const button = document.getElementById('testbutton')
+button.addEventListener("click", getinfo);
+
