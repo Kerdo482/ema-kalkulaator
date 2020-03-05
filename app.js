d16e7ff683ebb4f990a75e918ba6f7a7dc6b2443
Robin Kukk <robin.kukk@ametikool.ee>
2020-03-05 09:49:13 +0200

    added more jobs + basic history function

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-
 function getinfo(){
   // vars
   var e = document.getElementById("jobSelect");
   var selectnr = e.options[e.selectedIndex].value;
-
-  //var text = e.options[e.selectedIndex].text;
+  var test =  e.options[e.selectedIndex].innerHTML;
+  var currenthrs = 0
+  var currentvalue = 0
+  var testMath = 0
+  const statDiv = document.getElementById('root')
+  const addedtotalDiv = document.getElementById('addedtotal')
+  const totaldiv = document.getElementById('total')
+  const historydiv = document.getElementById('history')
+  
   var jsinput = document.getElementById('testinput')
-  currenthrs = jsinput.value
+      currenthrs = jsinput.value
   //fetch start
 fetch('http://andmebaas.stat.ee/sdmx-json/data/PA627/' + selectnr + '.1.1+2/all?startTime=2010&endTime=2014&dimensionAtObservation=allDimensions')
     .then((response) => {
     return response.json()
   })
   .then((data) => {
-    const statDiv = document.getElementById('root')
-    const totalDiv = document.getElementById('total')
-    const palkNr = data.dataSets[0].observations['0:0:0:1'][0]
+    var palkNr = data.dataSets[0].observations['0:0:0:1'][0]
     console.log(data)
     statDiv.innerHTML = palkNr
-    totalDiv.innerHTML = palkNr * currenthrs
+    testMath = palkNr * currenthrs
+    addedtotalDiv.innerHTML = testMath
+    currentvalue = currentvalue + testMath
+    totaldiv.innerHTML = currentvalue
+
+    //history
+    let newRow = document.createElement("div");
+    newRow.classList.add("print-in");
+    newRow.innerHTML = " " + test + " " + currenthrs + " tundi = " + currentvalue;
+    historydiv.appendChild(newRow);
+    
 
 
-      
 
     });
   }
+
+
   //button
 const button = document.getElementById('testbutton')
 button.addEventListener("click", getinfo);
