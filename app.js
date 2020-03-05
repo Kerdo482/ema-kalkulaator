083407f108d768350797642fafc9c7db26763274
hanspavelson <44016837+hanspavelson@users.noreply.github.com>
2020-03-05 14:49:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
+// vars
+const e = document.getElementById("jobSelect");
+
+let currenthrs = 0
+let currentvalue = 0
+let mathcalc = 0
+let nightmultipl = 1
+const statDiv = document.getElementById('root')
+const addedtotalDiv = document.getElementById('addedtotal')
+const totaldiv = document.getElementById('total')
+const historydiv = document.getElementById('history')
+const jsinput = document.getElementById('testinput')
+const checkBox = document.getElementById("myCheck");
+
+
+function myFunction() {
+  if (checkBox.checked == true){
+    nightmultipl = 1.5;
+  } else {
+    nightmultipl = 1;
+  }
+}
+
 
 function getinfo(){
-    // vars
-    var e = document.getElementById("jobSelect");
-    var selectnr = e.options[e.selectedIndex].value;
-  
-    //var text = e.options[e.selectedIndex].text;
-    var jsinput = document.getElementById('testinput')
-    currenthrs = jsinput.value
-    //fetch start
+  let selectnr = e.options[e.selectedIndex].value;
+  let jobname =  e.options[e.selectedIndex].innerHTML;
+  currenthrs = jsinput.value
+  //fetch start
   fetch('http://andmebaas.stat.ee/sdmx-json/data/PA627/' + selectnr + '.1.1+2/all?startTime=2010&endTime=2014&dimensionAtObservation=allDimensions')
-      .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-      const statDiv = document.getElementById('root')
-      const totalDiv = document.getElementById('total')
-      const palkNr = data.dataSets[0].observations['0:0:0:1'][0]
-      console.log(data)
-      statDiv.innerHTML = palkNr
-      totalDiv.innerHTML = palkNr * currenthrs
-  
-  
-        
-  
-      });
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    // ühe tunni palk
+    var palkNr = data.dataSets[0].observations['0:0:0:1'][0]
+    //statDiv.innerHTML =  (palkNr * 1.645).toFixed(2) + "€ Tunnipalk"
+    // palk * tunnid
+    mathcalc = (palkNr * currenthrs) * nightmultipl * 1.645
+    // total
+    currentvalue = currentvalue + mathcalc
+    totaldiv.innerHTML = currentvalue.toFixed(2)+ "€ Kokku"
+
+    //history
+    let newRow = document.createElement("div");
+    newRow.classList.add("print-in");
+    if (checkBox.checked == true){
+      newRow.innerHTML = " " + jobname + " " + currenthrs + " öötundi  " + '<div class="price">' + mathcalc.toFixed(2) + "€ </div>";
+    } else {
+      newRow.innerHTML = " " + jobname + " " + currenthrs + " tundi  " + '<div class="price">' + mathcalc.toFixed(2) + "€ </div>";
     }
-    //button
-  const button = document.getElementById('testbutton')
+    
+    historydiv.appendChild(newRow);
+
+
+  });
+}
+
+
+  //button
+  const button = document.getElementById('button')
   button.addEventListener("click", getinfo);
-  
-  
