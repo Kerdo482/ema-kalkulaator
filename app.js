9285911544f05e4e63960271f56e0b4d84373d0d
Robin Kukk <robin.kukk@ametikool.ee>
2020-03-05 10:28:00 +0200

    night shift + total added

--- app.js
+++ app.js
@@ -1,46 +1,64 @@
+// vars
+const e = document.getElementById("jobSelect");
+
+let currenthrs = 0
+let currentvalue = 0
+let testMath = 0
+let nightmultipl = 1
+const statDiv = document.getElementById('root')
+const addedtotalDiv = document.getElementById('addedtotal')
+const totaldiv = document.getElementById('total')
+const historydiv = document.getElementById('history')
+const jsinput = document.getElementById('testinput')
+const checkBox = document.getElementById("myCheck");
+
+
+function myFunction() {
+  if (checkBox.checked == true){
+    nightmultipl = 1.5;
+  } else {
+    nightmultipl = 1;
+  }
+}
+
+
 function getinfo(){
-  // vars
-  var e = document.getElementById("jobSelect");
-  var selectnr = e.options[e.selectedIndex].value;
-  var test =  e.options[e.selectedIndex].innerHTML;
-  var currenthrs = 0
-  var currentvalue = 0
-  var testMath = 0
-  const statDiv = document.getElementById('root')
-  const addedtotalDiv = document.getElementById('addedtotal')
-  const totaldiv = document.getElementById('total')
-  const historydiv = document.getElementById('history')
-  
-  var jsinput = document.getElementById('testinput')
-      currenthrs = jsinput.value
+  let selectnr = e.options[e.selectedIndex].value;
+  let test =  e.options[e.selectedIndex].innerHTML;
+  currenthrs = jsinput.value
   //fetch start
-fetch('http://andmebaas.stat.ee/sdmx-json/data/PA627/' + selectnr + '.1.1+2/all?startTime=2010&endTime=2014&dimensionAtObservation=allDimensions')
-    .then((response) => {
+  fetch('http://andmebaas.stat.ee/sdmx-json/data/PA627/' + selectnr + '.1.1+2/all?startTime=2010&endTime=2014&dimensionAtObservation=allDimensions')
+  .then((response) => {
     return response.json()
   })
   .then((data) => {
+    // ühe tunni palk
     var palkNr = data.dataSets[0].observations['0:0:0:1'][0]
-    console.log(data)
     statDiv.innerHTML = palkNr
-    testMath = palkNr * currenthrs
-    addedtotalDiv.innerHTML = testMath
+    // palk * tunnid
+    testMath = (palkNr * currenthrs) * nightmultipl
+    // total
     currentvalue = currentvalue + testMath
     totaldiv.innerHTML = currentvalue
 
     //history
     let newRow = document.createElement("div");
     newRow.classList.add("print-in");
-    newRow.innerHTML = " " + test + " " + currenthrs + " tundi = " + currentvalue;
-    historydiv.appendChild(newRow);
+    if (checkBox.checked == true){
+      newRow.innerHTML = " " + test + " " + currenthrs + " öötundi = " + testMath;
+    } else {
+      newRow.innerHTML = " " + test + " " + currenthrs + " tundi = " + testMath;
+    }
     
+    historydiv.appendChild(newRow);
 
 
-
-    });
-  }
+  });
+}
 
 
   //button
-const button = document.getElementById('testbutton')
-button.addEventListener("click", getinfo);
+  const button = document.getElementById('testbutton')
+  button.addEventListener("click", getinfo);
+
 
