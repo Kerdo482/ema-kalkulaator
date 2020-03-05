af7117b97cde604386c4543ff53f7fcda00d7d31
Robin Kukk <robin.kukk@ametikool.ee>
2020-03-05 10:46:00 +0200

    coefficient added, variable names changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const e = document.getElementById("jobSelect");
 
 let currenthrs = 0
 let currentvalue = 0
-let testMath = 0
+let mathcalc = 0
 let nightmultipl = 1
 const statDiv = document.getElementById('root')
 const addedtotalDiv = document.getElementById('addedtotal')
@@ -24,7 +24,7 @@ function myFunction() {
 
 function getinfo(){
   let selectnr = e.options[e.selectedIndex].value;
-  let test =  e.options[e.selectedIndex].innerHTML;
+  let jobname =  e.options[e.selectedIndex].innerHTML;
   currenthrs = jsinput.value
   //fetch start
   fetch('http://andmebaas.stat.ee/sdmx-json/data/PA627/' + selectnr + '.1.1+2/all?startTime=2010&endTime=2014&dimensionAtObservation=allDimensions')
@@ -34,20 +34,20 @@ function getinfo(){
   .then((data) => {
     // ühe tunni palk
     var palkNr = data.dataSets[0].observations['0:0:0:1'][0]
-    statDiv.innerHTML = palkNr
+    statDiv.innerHTML = (palkNr * 1.645).toFixed(2)
     // palk * tunnid
-    testMath = (palkNr * currenthrs) * nightmultipl
+    mathcalc = (palkNr * currenthrs) * nightmultipl * 1.645
     // total
-    currentvalue = currentvalue + testMath
-    totaldiv.innerHTML = currentvalue
+    currentvalue = currentvalue + mathcalc
+    totaldiv.innerHTML = currentvalue.toFixed(2)
 
     //history
     let newRow = document.createElement("div");
     newRow.classList.add("print-in");
     if (checkBox.checked == true){
-      newRow.innerHTML = " " + test + " " + currenthrs + " öötundi = " + testMath;
+      newRow.innerHTML = " " + jobname + " " + currenthrs + " öötundi = " + mathcalc.toFixed(2);
     } else {
-      newRow.innerHTML = " " + test + " " + currenthrs + " tundi = " + testMath;
+      newRow.innerHTML = " " + jobname + " " + currenthrs + " tundi = " + mathcalc.toFixed(2);
     }
     
     historydiv.appendChild(newRow);
@@ -58,7 +58,7 @@ function getinfo(){
 
 
   //button
-  const button = document.getElementById('testbutton')
+  const button = document.getElementById('button')
   button.addEventListener("click", getinfo);
 
 
